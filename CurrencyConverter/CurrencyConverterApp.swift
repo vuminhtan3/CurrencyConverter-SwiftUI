814af991363ebc407a7fdae90e0ea00df4aaa0dc
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-03-21 14:52:49 +0700

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Minh Tan Vu on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
