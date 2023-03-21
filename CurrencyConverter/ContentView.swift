557b2886b2224186d27cae3a6a31f903548041c5
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-03-22 00:35:44 +0700

    Complete essentially the Main screen

--- CurrencyConverter/ContentView.swift
+++ CurrencyConverter/ContentView.swift
@@ -9,13 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            //Background Image
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                //Prancing pony image
+                Image("prancingpony")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                //Currency Exchange text
+                Text("Currency Exchange")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                //Currency conversion section
+                HStack{
+                    //Left conversion section
+                    VStack {
+                        HStack {
+                            //Coin icon
+                            Image("silverpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            //Coin name
+                            Text("Silver piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        //Textfield
+                        Text("Textfield")
+                            .foregroundColor(.white)
+                        
+                    }
+                    //Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
+                    //Right conversion section
+                    VStack {
+                        HStack {
+                            //Coin icon
+                            Image("goldpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            //Coin name
+                            Text("Gold piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        
+                        //Textfield
+                        Text("Textfield")
+                            .foregroundColor(.white)
+                    }
+                }
+                
+                Spacer()
+                
+                //Info button
+                HStack{
+                    Spacer()
+                    
+                    Button{
+                        //Display exchange info screen
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding()
+                }
+            }
         }
-        .padding()
     }
 }
 
