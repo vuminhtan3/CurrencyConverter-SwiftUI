d8dce26cd15c21c217a10d2bfb85c3ef78afc321
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-03-22 23:54:18 +0700

    Complete design the Exchange Info screen and add @Environment property to dismiss the screen

--- CurrencyConverter/ExchangeInfo.swift
+++ CurrencyConverter/ExchangeInfo.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ExchangeInfo: View {
+    @Environment(\.dismiss) var dismiss
     var body: some View {
         ZStack {
             //Background
@@ -20,22 +21,11 @@ struct ExchangeInfo: View {
                 // Title
                 Text("Exchange Rates")
                     .font(.largeTitle)
-                    .padding(.bottom)
                 
                 //Contents
-                Text("""
-                    Here at the Prancing Pony, we are
-                    happy to offer you a place where you
-                    can exchange all the known
-                    currencies in the intire world, except
-                    ont. We used to take Brandy Bucks,
-                    but after finding out that it was a
-                    person instead of a piece of paper, we
-                    realized it had no value to us. Below is
-                    a simple guide to our currency
-                    exchange rates:
-                    """)
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the intire world, exceptont. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
                 .font(.title2)
+                .padding()
                 
                //Exchange Rate 1
                 ExchangeRate(leftImage: "goldpiece", rightImage: "goldpenny", exchangeRateText: "1 Gold Piece = 4 Gold Pennies")
@@ -50,16 +40,14 @@ struct ExchangeInfo: View {
                  ExchangeRate(leftImage: "silverpenny", rightImage: "copperpenny", exchangeRateText: "1 Silver Penny = 100 Copper Pennies")
                  
                 //Done Button
-                Button {
-                    
-                } label: {
-                    Text("Done")
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .padding(5)
+                Button ("Done"){
+                    dismiss()
                 }
-                .background(.brown)
-                .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding(5)
+                    .background(.brown)
+                    .cornerRadius(10)
             }
         }
     }
